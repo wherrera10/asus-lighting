64d726b8379d1be0afec5a9cd6d1e45bc245731f
Bill <wherrera10@users.noreply.github.com>
2015-09-26 13:20:54 -1000

    Create sound_to_light.py

---
+++ sound_to_light.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+IMPORTANT: RUN AS ADMINISTRATOR
+
+Python Tkinter app to set up audio based lighting
+
+Created on Sat Sep 26 13:13:03 2015
+
+@author: William
+"""
+
+# button to choose audio source, with instructions as a popup window
+
+# button to choose path to dll, with default
+
+# slider to choose timing for changes, with default 0.5 sec
+
+# button to start lights going
+
+# button to exit
